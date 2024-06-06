fb0690be1f075d25eaaa80e488fccbccc357206e
armaan.budhiraja2023@gmail.com <armaan.collegework@gmail.com>
2024-06-06 19:02:26 +0530

    Maid main window

---
+++ Main.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+
+Builder.load_file('main.kv')
+
+#Main window loop
+class MainApp(App):
+    def build(self):
+        pass
+
+if __name__=='__main__':
+    MainApp().run()
